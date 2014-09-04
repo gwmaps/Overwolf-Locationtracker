5791e32927d3d1c237f1f1a8f4c6924957137fc9
smiley <smiley@chillerlan.net>
2014-09-04 20:25:31 +0200

    initial commit

---
+++ backend/inc/config.inc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * config.inc.php
+ * created: 04.03.14
+ */
+
+
+// mysql connection data
+$mysql = [
+	'server'   => 'localhost',
+	'user'     => 'user',
+	'password' => 'password',
+	'dbname'   => 'dbname'
+];
+
+define('TABLE_WORLDS', 'gw2_worlds');
+define('TABLE_MAPS', 'gw2_maps');
+define('TABLE_GUILDS', 'gw2_guilds');
+
+define('TABLE_PLAYER_POS', 'gw2_player_pos');
